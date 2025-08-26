7bbde5a47cc81f624125427a836db46db88cb11a
UstubecEmre <emresb1999@gmail.com>
2025-08-26 13:45:08 +0300

    feat: implement WHERE and ORDER BY examples for Products

--- Northwind_SQL_Examples.sql
+++ Northwind_SQL_Examples.sql
@@ -4,7 +4,10 @@ USE Northwind
 
 
 --Soru 1:  Butun kullanicilari getiren sorgu
-SELECT * FROM Customers
+SELECT 
+	* 
+FROM 
+	Customers
 
 -- Soru 2: Sadece CompanyName, City ve Country getiren sorguyu yaziniz
 SELECT
@@ -12,15 +15,95 @@ SELECT
 	City [Sehir],
 	Country[Ulke]
 FROM
-Customers
+	Customers
 
 -- Soru 3: Mannheim sehrinde yasayan kisilerin ID, PostalCode bilgilerini listeleyin
 SELECT
-	CustomerID,
-	PostalCode
+	 CustomerID
+	,PostalCode
 FROM
-Customers
-WHERE City = 'Mannheim'
+	Customers
+WHERE 
+	City = 'Mannheim'
 
 
--- Soru 4: Orders tablosuna gecis yapin ve tum urunlerin adlarini ve fiyatlarini listeleyin.
+-- Soru 4: Products tablosuna gecis yapin ve tum urunlerin adlarini ve fiyatlarini listeleyin.
+
+SELECT 
+	*
+FROM 
+	Products
+
+
+SELECT
+	 ProductName [UrunIsmi]
+   , UnitPrice [UrunFiyati]
+FROM 
+	Products
+
+
+-- Soru 5: Products tablosunda UnitsStock alani 20'den buyuk olanlarin ismini ve stok miktarini getirin
+SELECT
+	ProductName[UrunAdi]
+  ,	UnitsInStock[StokAdedi]
+FROM
+	Products
+WHERE
+	UnitsInStock > 20
+
+
+-- Soru 6: Products tablosunda calismaya devam edelim. Boylece goz asinaligi olusur ve sorgulari yazmasi kolaylasir.
+-- Urunleri a'dan z'ye olacak sekilde siralayin
+
+SELECT
+	ProductName [UrunAdi]
+ ,  UnitPrice [UrunFiyati]
+FROM
+	Products
+ORDER BY
+	ProductName
+
+
+-- Soru 7: Urunleri stok miktari azalan sekilde siralayalim. Bu urunlerin urun ismi, stok adedi ve fiyati gosterilsin
+SELECT
+	ProductName [UrunAdi]
+  , UnitsInStock [StokAdedi]
+  , UnitPrice [UrunFiyati]
+FROM
+	Products
+ORDER BY
+	UnitsInStock DESC
+
+
+
+-- Soru 8: En yuksek fiyatli 10 urunu listeleyelim.
+SELECT TOP 10
+	ProductName [UrunAdi]
+  , UnitPrice [UrunFiyati]
+FROM
+	Products
+ORDER BY
+	UnitPrice DESC 
+
+
+-- Soru 9: En dusuk stoklu 10 urunu listeleyelim.
+SELECT TOP 10
+	ProductName [UrunAdi]
+  , UnitPrice [UrunFiyati]
+  , UnitsInStock [UrunStokAdedi]
+FROM
+	Products
+ORDER BY
+	UnitsInStock  
+
+
+-- Soru 10: Urunler tablosunda discountinued 1 olanlari listeleyelim 
+SELECT
+	ProductID [UrunID]
+  , ProductName [UrunIsmi]
+  , UnitsInStock [StokAdedi]
+  , UnitPrice[UrunFiyati]
+FROM
+	Products
+WHERE
+	Discontinued = 1 
